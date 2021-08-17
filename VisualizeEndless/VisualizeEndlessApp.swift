4f54444442f47eff0ac462359767a09f10b20741
eladioav <eladioav@hotmail.com>
2021-08-17 11:32:52 -0500

    Initial Commit

---
+++ VisualizeEndless/VisualizeEndlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualizeEndlessApp.swift
+//  VisualizeEndless
+//
+//  Created by Eladio Alvarez Valle on 17/08/21.
+//
+
+import SwiftUI
+
+@main
+struct VisualizeEndlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
